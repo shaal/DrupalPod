27c5ff21de793a70a25e6e2a892a62707fb863de
shaal <ofer@3paces.com>
2021-05-06 13:45:43 +0000

    Adding initial ddev-gitpod setup

---
+++ .gitpod/gitpod-setup-ddev.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# Set up ddev for use on gitpod
+
+set -eu -o pipefail
+
+MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Generate a config.gitpod.yaml that adds the gitpod
+# proxied ports so they're known to ddev.
+shortgpurl="${GITPOD_WORKSPACE_URL#'https://'}"
+
+cat <<CONFIGEND > ${MYDIR}/config.gitpod.yaml
+#ddev-gitpod-generated
+router_http_port: 8080
+router_https_port: 8443
+use_dns_when_possible: false
+
+additional_fqdns:
+- 8080-${shortgpurl}
+- 8025-${shortgpurl}
+- 8036-${shortgpurl}
+CONFIGEND
+
+# We need host.docker.internal inside the container,
+# So add it via docker-compose.host-docker-internal.yaml
+hostip=$(awk "\$2 == \"$HOSTNAME\" { print \$1; }" /etc/hosts)
+
+cat <<COMPOSEEND >${MYDIR}/docker-compose.host-docker-internal.yaml
+#ddev-gitpod-generated
+version: "3.6"
+services:
+  web:
+    extra_hosts:
+    - "host.docker.internal:${hostip}"
+COMPOSEEND
+
+# Misc housekeeping before start
+ddev config global --instrumentation-opt-in=true --router-bind-all-interfaces=true
+
+ddev start

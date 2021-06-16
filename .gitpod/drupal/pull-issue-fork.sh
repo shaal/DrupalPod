21a82b1f2dda9ba32ab0dd618acc3514b7042d11
shaal <ofer@3paces.com>
2021-06-16 04:00:35 +0000

    Supporting modules in pull-issue-fork

--- .gitpod/drupal/pull-issue-fork.sh
+++ .gitpod/drupal/pull-issue-fork.sh
@@ -22,7 +22,7 @@ fi
 # Set issue_fork
 issue_fork=$project_name-$issue_number
 
-# Clone project and set WORK_DIR
+# Set WORK_DIR
 if [ $project_type == "core" ]; then
     WORK_DIR="${GITPOD_REPO_ROOT}"/repos
 elif [ $project_type == "module" ]; then
@@ -33,17 +33,26 @@ elif [ $project_type == "theme" ]; then
     mkdir -p "${WORK_DIR}"
 fi
 
+# Clone project
 [ ! -d "${WORK_DIR}"/$project_name ] && cd "$WORK_DIR" && git clone https://git.drupalcode.org/project/$project_name
 WORK_DIR=$WORK_DIR/$project_name
 
+# If branch already exist only run checkout,
 if cd "${WORK_DIR}" && git show-ref -q --heads $branch_name; then
-    cd "${WORK_DIR}" && git checkout $branch_name
-else
     cd "${WORK_DIR}" && git remote add $issue_fork git@git.drupal.org:issue/$issue_fork.git
     cd "${WORK_DIR}" && git fetch $issue_fork
     cd "${WORK_DIR}" && git checkout -b $branch_name --track $issue_fork/$branch_name
+else
+    cd "${WORK_DIR}" && git checkout $branch_name
 fi
 
-if [ $project_type == "core" ]; then
-    ddev composer update
+# If project type is NOT core, change Drupal core version
+if [ $project_type != "core" ]; then
+    # If no Drupal core version set, use version 9.2.x by default 
+    if [ -z "$core_version" ]; then
+        core_version=9.2.x
+    fi
+    cd "${GITPOD_REPO_ROOT}"/repos/drupal && git checkout "${core_version}"
 fi
+
+ddev composer update

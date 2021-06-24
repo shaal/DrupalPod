2c4c268713599093246d37d1060f9c29adf5d522
shaal <ofer@3paces.com>
2021-06-24 13:48:38 +0000

    restructure file locations

--- .gitpod/drupal/ssh/03-confirm-ssh-setup.sh
+++ .gitpod/drupal/ssh/03-confirm-ssh-setup.sh
@@ -2,12 +2,12 @@
 
 # Only if ssh connection works (private and public keys match) - save Gitpod environment variable
 
-ssh -T git@git.drupal.org
+ssh -T git@git.drupal.org -y
 if [ $? -eq 0 ]; then
     echo "Setup was succesful, saving your private key in Gitpod"
     # Set Gitpod variable anvironment
 
-    # Due to bug in gp env command, replace `=` with `_`
+    # Due to bug in gp env command, replace `=` with `_` - https://github.com/gitpod-io/gitpod/issues/4493
     DRUPAL_SSH_KEY=$(sed 's/=/_/g' ~/.ssh/id_rsa)
     gp env "DRUPAL_SSH_KEY=$DRUPAL_SSH_KEY" > /dev/null
     # Copy key to /workspace in case this workspace times out
@@ -17,9 +17,9 @@ else
         echo "Setup failed, create private key again"
         rm -f /workspace/id_rsa
         rm -rf ~/.ssh
-        .gitpod/drupal/setup-private-ssh.sh
+        .gitpod/drupal/ssh/01-setup-private-ssh.sh
     else
         echo "Setup failed, please confirm you copied public key to Drupal"
-        .gitpod/drupal/drupal-instructions.sh
+        .gitpod/drupal/ssh/02-generate-drupal-ssh-instructions.sh
     fi
 fi

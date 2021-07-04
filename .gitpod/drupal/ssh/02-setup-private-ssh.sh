7507793b12b4eb716cfad258cea3394b363ce509
shaal <ofer@3paces.com>
2021-07-04 05:24:19 +0000

    Change variable name + if statement

--- .gitpod/drupal/ssh/02-setup-private-ssh.sh
+++ .gitpod/drupal/ssh/02-setup-private-ssh.sh
@@ -3,7 +3,7 @@ if $DEBUG_DRUPALPOD; then
     set -x
 fi
 
-SSH_SETUP=true
+SSH_SETUP_REQUIRED=true
 
 # Add git.drupal.org to known_hosts
 mkdir -p ~/.ssh
@@ -13,11 +13,11 @@ ssh-keyscan git.drupal.org >> ~/.ssh/known_hosts
 if ssh -T git@git.drupal.org; then
       read -r -p "SSH key was already confirmed with Drupal.org, are you sure you want to recreate SSH key? [y/N]" setup_ssh
       if [ "$setup_ssh" != "y" ] && [ "$setup_ssh" != "Y" ]; then
-            SSH_SETUP=false
+            SSH_SETUP_REQUIRED=false
       fi
 fi
 
-if [ "$SSH_SETUP" ]; then
+if $SSH_SETUP_REQUIRED; then
       # Create ssh key pairing + instructions to paste public key in drupal.org
       ssh-keygen -q -t rsa -b 4096 -f ~/.ssh/id_rsa
       .gitpod/drupal/ssh/03-generate-drupal-ssh-instructions.sh

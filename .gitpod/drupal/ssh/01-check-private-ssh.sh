c6373c4b07623654efbec869775d415d589ead19
shaal <ofer@3paces.com>
2021-07-10 15:04:58 +0000

    No need to convert chars when loading back gp env

--- .gitpod/drupal/ssh/01-check-private-ssh.sh
+++ .gitpod/drupal/ssh/01-check-private-ssh.sh
@@ -19,7 +19,7 @@ else
             echo "Setting SSH key from environment variable"
             mkdir -p ~/.ssh
             # Due to bug in gp env command, replace `=` with `_` - https://github.com/gitpod-io/gitpod/issues/4493
-            printenv DRUPAL_SSH_KEY | sed 's/_/=/g' >  ~/.ssh/id_rsa
+            printenv DRUPAL_SSH_KEY > ~/.ssh/id_rsa
             chmod 600 ~/.ssh/id_rsa
       fi
       # Ask for SSH keyphrase only once

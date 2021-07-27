da08c83cc3372f176fb88f9ad6480887133ea131
shaal <ofer@3paces.com>
2021-07-27 11:14:25 +0000

    update setting repo as ssh - new project structure

--- .gitpod/drupal/ssh/05-set-repo-as-ssh.sh
+++ .gitpod/drupal/ssh/05-set-repo-as-ssh.sh
@@ -3,22 +3,24 @@ if [ -n "$DEBUG_DRUPALPOD" ]; then
     set -x
 fi
 
-# Set WORK_DIR
-if [ "$DP_PROJECT_TYPE" == "project_core" ]; then
-    RELATIVE_WORK_DIR=repos
-    WORK_DIR="${GITPOD_REPO_ROOT}"/"$RELATIVE_WORK_DIR"
-    mkdir -p "${WORK_DIR}"
-elif [ "$DP_PROJECT_TYPE" == "project_module" ]; then
-    RELATIVE_WORK_DIR=web/modules/contrib
-    WORK_DIR="${GITPOD_REPO_ROOT}"/"$RELATIVE_WORK_DIR"
-    mkdir -p "${WORK_DIR}"
-elif [ "$DP_PROJECT_TYPE" == "project_theme" ]; then
-    RELATIVE_WORK_DIR=web/themes/contrib
-    WORK_DIR="${GITPOD_REPO_ROOT}"/"$RELATIVE_WORK_DIR"
-    mkdir -p "${WORK_DIR}"
+# Default settings (latest stable Drupal core)
+if [ -z "$DP_PROJECT_TYPE" ]; then
+    DP_PROJECT_TYPE=project_core
 fi
 
-RELATIVE_WORK_DIR=$RELATIVE_WORK_DIR/$DP_PROJECT_NAME
+# Set WORK_DIR
+if [ "$DP_PROJECT_TYPE" == "project_core" ]; then
+    BASE_PROJECT_DIR=web/core
+    WORK_DIR="${GITPOD_REPO_ROOT}"/"$BASE_PROJECT_DIR"
+elif [ "$DP_PROJECT_TYPE" == "project_module" ]; then
+    BASE_PROJECT_DIR=web/modules/contrib
+    WORK_DIR="${GITPOD_REPO_ROOT}"/"$BASE_PROJECT_DIR"
+elif [ "$DP_PROJECT_TYPE" == "project_theme" ]; then
+    BASE_PROJECT_DIR=web/themes/contrib
+    WORK_DIR="${GITPOD_REPO_ROOT}"/"$BASE_PROJECT_DIR"
+fi
+
+RELATIVE_WORK_DIR=$BASE_PROJECT_DIR/$DP_PROJECT_NAME
 WORK_DIR="${GITPOD_REPO_ROOT}"/$RELATIVE_WORK_DIR
 
 # Set a specific branch if there's issue_fork

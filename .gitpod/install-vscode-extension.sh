25d488e32bdcb375d8925f3d59b7820d6c9e2110
Matthew Radcliffe <mradcliffe@softpixel.com>
2021-07-30 21:04:22 +0000

    Adds showing guide on startup

--- .gitpod/install-vscode-extension.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-code --install-extension /workspace/DrupalPod/repos/drupalpod-ext/drupalpod-ext-0.0.1.visx
--- repos/drupalpod-ext/src/extension.ts
+++ repos/drupalpod-ext/src/extension.ts
@@ -1,16 +1,20 @@
 import * as vscode from 'vscode';
 
-export function activate(context: vscode.ExtensionContext) {
-	context.subscriptions.push(
-		vscode.commands.registerCommand('drupalpod-ext.start', () => {
-			const panel = vscode.window.createWebviewPanel(
-				'drupalpod-ext',
-				'Drupal Contributions',
-				vscode.ViewColumn.One,
-				{}
-			);
-		})
-	);
+export async function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    vscode.commands.registerCommand('drupalpod-ext.start', () => {
+      const panel = vscode.window.createWebviewPanel(
+        'drupalpod-ext',
+        'Contribution Guide',
+        vscode.ViewColumn.One,
+        {}
+      );
+    })
+  );
+
+  if (!vscode.workspace.getConfiguration('drupalpod.hideOnStartup')) {
+    await vscode.commands.executeCommand('drupalpod-ext.start');
+  }
 }
 
 // this method is called when your extension is deactivated

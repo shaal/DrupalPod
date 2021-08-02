cc149125fe35a7198bb5598910222676ae683ddc
shaal <ofer@3paces.com>
2021-08-02 01:31:13 +0000

    Setup phpstorm command when starting a workspace

--- .gitpod/env-setup.sh
+++ .gitpod/env-setup.sh
@@ -3,8 +3,5 @@ if [ -n "$DEBUG_DRUPALPOD" ]; then
     set -x
 fi
 
-# Run initial setup for special Drupal core structure (clone Drupal core repo during prebuild)
-ddev composer run-script post-root-package-install
-
 # Create a phpstorm command
 sudo cp "${GITPOD_REPO_ROOT}"/.gitpod/phpstorm.template.sh /usr/local/bin/phpstorm

f37f90bfdc8ba9530aa4ab09a4fa61b356f41281
shaal <ofer@3paces.com>
2021-08-28 00:18:46 +0000

    Adjust settings to the new omit router feature

--- .gitpod/ddev-in-gitpod-setup.sh
+++ .gitpod/ddev-in-gitpod-setup.sh
@@ -18,14 +18,13 @@ fi
 cat <<CONFIGEND > "${DDEV_DIR}"/config.gitpod.yaml
 #ddev-gitpod-generated
 php_version: "$DP_PHP_VERSION"
-use_dns_when_possible: false
-# Throwaway ports, otherwise Gitpod throw an error 'port needs to be > 1024'
-router_http_port: "8888"
-router_https_port: "8889"
-additional_fqdns:
-- 8888-${shortgpurl}
-- 8025-${shortgpurl}
-- 8036-${shortgpurl}
+
+bind_on_all_interfaces: true
+host_webserver_port: 8080
+# Will ignore the direct-bind https port, which will land on 2222
+host_https_port: 2222
+# Allows local db clients to run
+host_db_port: 3306
 CONFIGEND
 
 # We need host.docker.internal inside the container,
@@ -41,12 +40,7 @@ services:
       - DRUSH_OPTIONS_URI=$(gp url 8080)
     extra_hosts:
     - "host.docker.internal:${hostip}"
-    # This adds 8080 on the host (bound on all interfaces)
-    # It goes directly to the web container without
-    # ddev-nginx
-    ports:
-    - 8080:80
 COMPOSEEND
 
 # Misc housekeeping before start
-ddev config global --instrumentation-opt-in=true --router-bind-all-interfaces=true
+ddev config global --instrumentation-opt-in=true --omit-containers=ddev-router

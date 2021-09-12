954c952f197599d278ebedb12b4813411ae46a49
Ofer Shaal <shaalofer@gmail.com>
2021-09-11 23:39:02 -0400

    Improve phpstorm experience

--- .gitpod/phpstorm.template.sh
+++ .gitpod/phpstorm.template.sh
@@ -6,4 +6,9 @@ fi
 if [ ! -x ~/.projector/configs/PhpStorm/run.sh ]; then
   echo "PhpStorm runner not found" && exit 1
 fi
+
+# When port 9999 is ready - open that port in a new browser tab
+gp await-port 9999 && gp preview "$(gp url 9999)" --external &
+
+# Run PHPStorm
 ~/.projector/configs/PhpStorm/run.sh "$GITPOD_REPO_ROOT"

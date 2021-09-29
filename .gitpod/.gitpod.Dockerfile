ad95053306f54cee36b9e87f5cd34cae5a8737e4
Ofer Shaal <shaalofer@gmail.com>
2021-09-29 11:33:36 -0400

    Utilize ddev's new omit-router feature (DDEV v1.18)
    * Adjust settings to the new omit router feature
    * Added specific ports to ignore
    * Configure MailHog and phpMyAdmin

--- .gitpod/.gitpod.Dockerfile
+++ .gitpod/.gitpod.Dockerfile
@@ -8,7 +8,7 @@ RUN sudo apt-get -qq install -y patchutils python3 python3-pip libxext6 libxrend
 RUN pip3 install projector-installer
 # Install PhpStorm
 RUN mkdir -p ~/.projector/configs  # Prevents projector install from asking for the license acceptance
-RUN projector install 'PhpStorm 2020.3.2' --no-auto-run
+RUN projector install 'PhpStorm 2020.3.3' --no-auto-run
 
 # Install ddev
 RUN brew update && brew install drud/ddev/ddev
--- .gitpod/ddev-in-gitpod-setup.sh
+++ .gitpod/ddev-in-gitpod-setup.sh
@@ -18,14 +18,17 @@ fi
 cat <<CONFIGEND > "${DDEV_DIR}"/config.gitpod.yaml
 #ddev-gitpod-generated
 php_version: "$DP_PHP_VERSION"
-use_dns_when_possible: false
-# Throwaway ports, otherwise Gitpod throw an error 'port needs to be > 1024'
-router_http_port: "8888"
-router_https_port: "8889"
-additional_fqdns:
-- 8888-${shortgpurl}
-- 8025-${shortgpurl}
-- 8036-${shortgpurl}
+
+bind_all_interfaces: true
+host_webserver_port: 8080
+# Will ignore the direct-bind https port, which will land on 2222
+host_https_port: 2222
+# Allows local db clients to run
+host_db_port: 3306
+# Assign MailHog port
+host_mailhog_port: "8025"
+# Assign phpMyAdmin port
+host_phpmyadmin_port: 8036
 CONFIGEND
 
 # We need host.docker.internal inside the container,
@@ -41,12 +44,7 @@ services:
       - DRUSH_OPTIONS_URI=$(gp url 8080)
     extra_hosts:
     - "host.docker.internal:${hostip}"
-    # This adds 8080 on the host (bound on all interfaces)
-    # It goes directly to the web container without
-    # ddev-nginx
-    ports:
-    - 8080:80
 COMPOSEEND
 
 # Misc housekeeping before start
-ddev config global --instrumentation-opt-in=true --router-bind-all-interfaces=true
+ddev config global --instrumentation-opt-in=true --omit-containers=ddev-router

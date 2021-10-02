5d7ba2b1c476dc53a08f81c4821678f3747da2e5
shaal <ofer@3paces.com>
2021-10-02 03:14:07 +0000

    During prebuild - only download ready-made envs

---
+++ .gitpod/drupal/create-environments.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+if [ -n "$DEBUG_DRUPALPOD" ] || [ -n "$GITPOD_HEADLESS" ]; then
+    set -x
+fi
+
+allProfiles=(minimal standard demo_umami)
+
+WORK_DIR="/workspace/ready-made-envs"
+mkdir -p "$WORK_DIR"
+
+# Read all Drupal supported versions from a file into an array
+readarray -t allDrupalSupportedVersions < "${GITPOD_REPO_ROOT}"/.gitpod/drupal/all-drupal-supported-versions.txt
+
+# Run through each Drupal Supported Versions - a
+# Install minimal, standard and umami profiles
+# Create a ddev snapshot
+
+for d in "${allDrupalSupportedVersions[@]}"; do
+  echo *** composer install
+  composer create-project drupal/recommended-project:"$d" "$WORK_DIR"/"$d"
+
+  # Install Drush
+  cd "$WORK_DIR"/"$d" && \
+    composer require \
+      drupal/admin_toolbar \
+      drush/drush:^10 \
+      drupal/coder \
+      drupal/devel
+
+  # Create ddev project name without the tilde character ~
+  cd "$WORK_DIR"/"$d" && ddev config --project-name latest-"${d/\~/}"
+
+  for p in "${allProfiles[@]}"; do
+    echo Building drupal-"$d"-"$p"
+
+    echo *** Drush Site Install
+    cd "$WORK_DIR"/"$d" && \
+
+    ddev drush si -y --account-pass=admin --site-name="DrupalPod" "$p"
+
+    echo *** Adding extra modules
+    cd "$WORK_DIR"/"$d"  && \
+      ddev drush en -y admin_toolbar devel
+
+    # Enable Claro as default admin theme
+    echo *** Enable Claro theme
+    cd "$WORK_DIR"/"$d" && \
+      ddev drush then claro && \
+      ddev drush config-set -y system.theme admin claro
+
+    echo *** Save a ddev snapshot
+    cd "$WORK_DIR"/"$d" && ddev snapshot -n drupal-"$d"-"$p"
+  done
+done
+
+# compress all environments into a file
+cd /workspace &&
+  tar czvf ready-made-envs.tar.gz ready-made-envs
--- .gitpod/drupal/prepare-environments.sh
+++ .gitpod/drupal/prepare-environments.sh
@@ -3,52 +3,11 @@ if [ -n "$DEBUG_DRUPALPOD" ] || [ -n "$GITPOD_HEADLESS" ]; then
     set -x
 fi
 
-allProfiles=(minimal standard demo_umami)
+# Download the ready-made envs file
+file=$(<"$GITPOD_REPO_ROOT"/.gitpod/drupal/ready-made-envs-url.txt)
+echo "*** Downloading all the environments"
+cd /workspace && wget "$file" > /dev/null
 
-WORK_DIR="$GITPOD_REPO_ROOT"/ready-made-envs
-mkdir -p "$WORK_DIR"
-
-# Read all Drupal supported versions from a file into an array
-readarray -t allDrupalSupportedVersions < "${GITPOD_REPO_ROOT}"/.gitpod/drupal/all-drupal-supported-versions.txt
-
-# Run through each Drupal Supported Versions - a
-# Install minimal, standard and umami profiles
-# Create a ddev snapshot
-
-for d in "${allDrupalSupportedVersions[@]}"; do
-  echo *** composer install
-  composer create-project drupal/recommended-project:"$d" "$WORK_DIR"/"$d"
-
-  # Install Drush
-  cd "$WORK_DIR"/"$d" && \
-    composer require \
-      drupal/admin_toolbar \
-      drush/drush:^10 \
-      drupal/coder \
-      drupal/devel
-
-  # Create ddev project name without the tilde character ~
-  cd "$WORK_DIR"/"$d" && ddev config --project-name latest-"${d/\~/}"
-
-  for p in "${allProfiles[@]}"; do
-    echo Building drupal-"$d"-"$p"
-
-    echo *** Drush Site Install
-    cd "$WORK_DIR"/"$d" && \
-
-    ddev drush si -y --account-pass=admin --site-name="DrupalPod" "$p"
-
-    echo *** Adding extra modules
-    cd "$WORK_DIR"/"$d"  && \
-      ddev drush en -y admin_toolbar coder devel
-
-    # Enable Claro as default admin theme
-    echo *** Enable Claro theme
-    cd "$WORK_DIR"/"$d" && \
-      ddev drush then claro && \
-      ddev drush config-set -y system.theme admin claro
-
-    echo *** Save a ddev snapshot
-    cd "$WORK_DIR"/"$d" && ddev snapshot -n drupal-"$d"-"$p"
-  done
-done
+# Extact the file
+echo "*** Extracting the environments (less than 1 minute)"
+cd /workspace && tar xzvf ready-made-envs.tar.gz > /dev/null

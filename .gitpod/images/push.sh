ea1456e074319c43e8564b4e47d728a0957eaf81
shaal <ofer@3paces.com>
2021-12-08 22:02:52 +0000

    rebuilding docker image (ddev 1.18.2)

--- .gitpod/images/push.sh
+++ .gitpod/images/push.sh
@@ -9,7 +9,7 @@ set -eu -o pipefail
 
 # "%Y-%m-%d"
 TODAY=$(date +"%Y%m%d")
-DOCKER_REPO=drupalpod/drupalpod-gitpod-base:"$TODAY"-base-experimental4
+DOCKER_REPO=drupalpod/drupalpod-gitpod-base:"$TODAY"-optimized
 
 echo "Pushing ${DOCKER_REPO}"
 set -x

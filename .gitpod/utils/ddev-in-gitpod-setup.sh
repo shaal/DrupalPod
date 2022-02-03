7177d558397c3490c76796b494f187556abd3ebb
Ofer Shaal <shaalofer@gmail.com>
2022-02-03 14:30:49 +0000

    set PHP version in ddev

--- .gitpod/utils/ddev-in-gitpod-setup.sh
+++ .gitpod/utils/ddev-in-gitpod-setup.sh
@@ -3,10 +3,18 @@ if [ -n "$DEBUG_DRUPALPOD" ] || [ -n "$GITPOD_HEADLESS" ]; then
     set -x
 fi
 
-# Set the default PHP version to 7.4
+# Set the default PHP version to 8.0
 if [ -z "$DP_PHP_VERSION" ]; then
-  DP_PHP_VERSION="7.4"
+  DP_PHP_VERSION="8.0"
 fi
+
+DDEV_DIR="${GITPOD_REPO_ROOT}/.ddev"
+# Generate a config.gitpod.yaml to allow for different PHP versions.
+
+cat <<CONFIGEND > "${DDEV_DIR}"/config.gitpod.yaml
+#ddev-gitpod-generated
+php_version: "$DP_PHP_VERSION"
+CONFIGEND
 
 # Misc housekeeping before start
 ddev config global --instrumentation-opt-in=false

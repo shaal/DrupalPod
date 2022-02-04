8d4d86f80ca568a9951cf556b1a95ac7f0a25bf4
Ofer Shaal <shaalofer@gmail.com>
2022-02-04 06:19:36 +0000

    main branch is default if ready-made-env not found

--- .gitpod/drupal/envs-prep/prepare-environments-gcs.sh
+++ .gitpod/drupal/envs-prep/prepare-environments-gcs.sh
@@ -13,12 +13,20 @@ fi
 mc config host add gcs https://storage.googleapis.com "$DP_GOOGLE_ACCESS_KEY" "$DP_GOOGLE_SECRET"
 
 CURRENT_BRANCH="$(cd "$GITPOD_REPO_ROOT" && git branch --show-current)"
+READY_MADE_ENV_SOURCE=''
 
 # Check if ready-made envs file exist
 if mc find gcs/drupalpod/"$CURRENT_BRANCH"/ready-made-envs.tar.gz; then
+    READY_MADE_ENV_SOURCE=gcs/drupalpod/"$CURRENT_BRANCH"/ready-made-envs.tar.gz
+elif mc find gcs/drupalpod/main/ready-made-envs.tar.gz; then
+    READY_MADE_ENV_SOURCE=gcs/drupalpod/"$CURRENT_BRANCH"/ready-made-envs.tar.gz
+fi
+
+# Check if ready-made envs file exist
+if [ -n "$READY_MADE_ENV_SOURCE" ]; then
     # Download the ready-made envs file
     echo "*** Downloading all the environments"
-    mc cp gcs/drupalpod/"$CURRENT_BRANCH"/ready-made-envs.tar.gz /workspace/ready-made-envs.tar.gz
+    mc cp "$READY_MADE_ENV_SOURCE" /workspace/ready-made-envs.tar.gz
 
     # Extact the file
     echo "*** Extracting the environments (less than 1 minute)"

a9ec1b7e0c2aacfcb29518d0a9c769894cbf3ff1
Ofer Shaal <shaalofer@gmail.com>
2022-02-09 03:30:38 +0000

    Create symlink for projects without composer.json

--- src/composer-drupal-core-setup/patch-core-and-drush.sh
+++ src/composer-drupal-core-setup/patch-core-and-drush.sh
@@ -4,4 +4,4 @@ if [ -n "$DEBUG_DRUPALPOD" ] || [ -n "$GITPOD_HEADLESS" ]; then
 fi
 
 git apply --directory=web -v src/composer-drupal-core-setup/scaffold-patch-index-and-update-php.patch
-git apply -v src/composer-drupal-core-setup/drush-cr-when-core-is-symlinked.patch
+# git apply -v src/composer-drupal-core-setup/drush-cr-when-core-is-symlinked.patch

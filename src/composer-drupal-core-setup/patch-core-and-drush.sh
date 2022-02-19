191c6e91606b2d6004e044a246f961a413202110
Ofer Shaal <shaalofer@gmail.com>
2022-02-19 12:48:35 +0000

    patch drush cr correctly

--- src/composer-drupal-core-setup/patch-core-and-drush.sh
+++ src/composer-drupal-core-setup/patch-core-and-drush.sh
@@ -4,4 +4,8 @@ if [ -n "$DEBUG_SCRIPT" ] || [ -n "$GITPOD_HEADLESS" ]; then
 fi
 
 git apply --directory=web -v src/composer-drupal-core-setup/scaffold-patch-index-and-update-php.patch
-# git apply -v src/composer-drupal-core-setup/drush-cr-when-core-is-symlinked.patch
+
+# Run patch for Drush only once
+if [ ! -f /workspace/drupalpod_initiated.status ]; then
+    git apply -v src/composer-drupal-core-setup/drush-cr-when-core-is-symlinked.patch
+fi

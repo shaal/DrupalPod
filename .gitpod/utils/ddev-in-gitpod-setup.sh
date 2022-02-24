756fa86180cf1832eb9ea1987b765c57500b8599
Ofer Shaal <shaalofer@gmail.com>
2022-02-24 22:15:37 +0000

    do not generate gitpod config file

--- .gitpod/utils/ddev-in-gitpod-setup.sh
+++ .gitpod/utils/ddev-in-gitpod-setup.sh
@@ -8,13 +8,5 @@ if [ -z "$DP_PHP_VERSION" ]; then
   DP_PHP_VERSION="8.1"
 fi
 
-DDEV_DIR="${GITPOD_REPO_ROOT}/.ddev"
-# Generate a config.gitpod.yaml to allow for different PHP versions.
-
-cat <<CONFIGEND > "${DDEV_DIR}"/config.gitpod.yaml
-#ddev-gitpod-generated
-php_version: "$DP_PHP_VERSION"
-CONFIGEND
-
 # Misc housekeeping before start
 ddev config global --instrumentation-opt-in=false

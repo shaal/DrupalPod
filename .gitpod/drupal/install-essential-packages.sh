e2b7993bfb2d35bf03af916f71db1848c33ebbda
Ofer Shaal <shaalofer@gmail.com>
2022-02-26 06:39:00 +0000

    only install devel when it's required (not in D10)

--- .gitpod/drupal/install-essential-packages.sh
+++ .gitpod/drupal/install-essential-packages.sh
@@ -3,15 +3,12 @@ if [ -n "$DEBUG_SCRIPT" ] || [ -n "$GITPOD_HEADLESS" ]; then
     set -x
 fi
 
-# Install Drush + Coder
-cd "${GITPOD_REPO_ROOT}" && time ddev composer require drush/drush drupal/coder
+# Install devel and admin_toolbar modules
+if [ "$DP_EXTRA_DEVEL" != '1' ]; then
+    DEVEL_PACKAGE=''
+fi
+if [ "$DP_EXTRA_ADMIN_TOOLBAR" != '1' ]; then
+    ADMIN_TOOLBAR_PACKAGE=''
+fi
 
-# Download extra modules
-if [ -n "$DP_EXTRA_DEVEL" ]; then
-    cd "${GITPOD_REPO_ROOT}" && \
-    ddev composer require "$DEVEL_PACKAGE"
-fi
-if [ -n "$DP_EXTRA_ADMIN_TOOLBAR" ]; then
-    cd "${GITPOD_REPO_ROOT}" && \
-    ddev composer require "$ADMIN_TOOLBAR_PACKAGE"
-fi
+cd "${GITPOD_REPO_ROOT}" && time ddev composer require drush/drush drupal/coder "$DEVEL_PACKAGE" "$ADMIN_TOOLBAR_PACKAGE"

f1ef798447499ee985453a3990db17333c1ed9d8
Ofer Shaal <shaalofer@gmail.com>
2022-02-26 02:21:10 -0500

    Various fixes (#73)
    * add a reusable script to install programs
    * improve patching drush only once
    * create environments with php 8.0
    * only install devel when it's required (not in D10)
    * fix packages that should be installed

---
+++ .gitpod/drupal/install-essential-packages.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+if [ -n "$DEBUG_SCRIPT" ] || [ -n "$GITPOD_HEADLESS" ]; then
+    set -x
+fi
+
+# Install devel and admin_toolbar modules
+if [ "$DP_EXTRA_DEVEL" != '1' ]; then
+    unset DEVEL_PACKAGE
+fi
+if [ "$DP_EXTRA_ADMIN_TOOLBAR" != '1' ]; then
+    unset ADMIN_TOOLBAR_PACKAGE
+fi
+
+cd "${GITPOD_REPO_ROOT}" && time ddev composer require drush/drush:^11.0 drupal/coder:^8.3 $DEVEL_PACKAGE $ADMIN_TOOLBAR_PACKAGE

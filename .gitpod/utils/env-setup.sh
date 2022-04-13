a922793568bd819729e27e030250d8d124dbb97b
Ofer Shaal <shaalofer@gmail.com>
2022-04-13 16:53:47 +0000

    Allow running drush, composer & yarn in ddev

--- .gitpod/utils/env-setup.sh
+++ .gitpod/utils/env-setup.sh
@@ -14,3 +14,12 @@ sudo cp "${GITPOD_REPO_ROOT}"/.gitpod/utils/protect-my-git.template.sh /usr/loca
 
 # Create php command (run php inside ddev container)
 sudo cp "${GITPOD_REPO_ROOT}"/.gitpod/utils/ddev-php.template.sh /usr/local/bin/php
+
+# Create drush command (run drush inside ddev container)
+sudo cp "${GITPOD_REPO_ROOT}"/.gitpod/utils/ddev-drush.template.sh /usr/local/bin/drush
+
+# Create yarn command (run yarn inside ddev container)
+sudo cp "${GITPOD_REPO_ROOT}"/.gitpod/utils/ddev-yarn.template.sh /usr/local/bin/yarn
+
+# Create composer command (run composer inside ddev container)
+sudo cp "${GITPOD_REPO_ROOT}"/.gitpod/utils/ddev-composer.template.sh /usr/local/bin/composer

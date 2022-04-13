c74dd5da07fd6607222d6211b7678ecb4376ba5b
Ofer Shaal <shaalofer@gmail.com>
2022-04-13 21:20:16 +0000

    add node, npx, nvm + restructure folders

--- .gitpod/utils/env-setup.sh
+++ .gitpod/utils/env-setup.sh
@@ -3,23 +3,34 @@ if [ -n "$DEBUG_SCRIPT" ]; then
     set -x
 fi
 
+cd "${GITPOD_REPO_ROOT}"/.gitpod/utils/script-templates || exit
+
 # Create a phpstorm command
-sudo cp "${GITPOD_REPO_ROOT}"/.gitpod/utils/phpstorm.template.sh /usr/local/bin/phpstorm
+sudo cp phpstorm.template.sh /usr/local/bin/phpstorm
 
 # Create a preview command
-sudo cp "${GITPOD_REPO_ROOT}"/.gitpod/utils/preview.template.sh /usr/local/bin/preview
+sudo cp preview.template.sh /usr/local/bin/preview
 
 # Create a protect-my-git command
-sudo cp "${GITPOD_REPO_ROOT}"/.gitpod/utils/protect-my-git.template.sh /usr/local/bin/protect-my-git
+sudo cp protect-my-git.template.sh /usr/local/bin/protect-my-git
 
 # Create php command (run php inside ddev container)
-sudo cp "${GITPOD_REPO_ROOT}"/.gitpod/utils/ddev-php.template.sh /usr/local/bin/php
+sudo cp ddev-php.template.sh /usr/local/bin/php
 
 # Create drush command (run drush inside ddev container)
-sudo cp "${GITPOD_REPO_ROOT}"/.gitpod/utils/ddev-drush.template.sh /usr/local/bin/drush
+sudo cp ddev-drush.template.sh /usr/local/bin/drush
 
 # Create yarn command (run yarn inside ddev container)
-sudo cp "${GITPOD_REPO_ROOT}"/.gitpod/utils/ddev-yarn.template.sh /usr/local/bin/yarn
+sudo cp ddev-yarn.template.sh /usr/local/bin/yarn
 
 # Create composer command (run composer inside ddev container)
-sudo cp "${GITPOD_REPO_ROOT}"/.gitpod/utils/ddev-composer.template.sh /usr/local/bin/composer
+sudo cp ddev-composer.template.sh /usr/local/bin/composer
+
+# Create node command (run composer inside ddev container)
+sudo cp ddev-node.template.sh /usr/local/bin/node
+
+# Create nvm command (run composer inside ddev container)
+sudo cp ddev-nvm.template.sh /usr/local/bin/nvm
+
+# Create npx command (run composer inside ddev container)
+sudo cp ddev-npx.template.sh /usr/local/bin/npx

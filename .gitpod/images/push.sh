0c7e77296663b89cc97fdab267853c5b8446f0de
Ofer Shaal <shaalofer@gmail.com>
2022-05-07 23:27:47 +0000

    Create and use tag 'latest'

--- .gitpod/images/push.sh
+++ .gitpod/images/push.sh
@@ -9,14 +9,17 @@ set -eu -o pipefail
 
 # "%Y-%m-%d"
 TODAY=$(date +"%Y%m%d")
-DOCKER_REPO=drupalpod/drupalpod-gitpod-base:"$TODAY"
+REPO_NAME=drupalpod/drupalpod-gitpod-base
+DOCKER_REPO="$REPO_NAME":"$TODAY"
+DOCKER_REPO_LATEST="$REPO_NAME":latest
 
 echo "Pushing ${DOCKER_REPO}"
 set -x
 # Build only current architecture and load into docker
 # docker buildx build -t "${DOCKER_REPO}" --push --target=drupalpod-gitpod-base --platform=linux/amd64 .
-docker build --target drupalpod-gitpod-base -t "${DOCKER_REPO}" .
+docker build --target drupalpod-gitpod-base -t "${DOCKER_REPO}" -t "${DOCKER_REPO_LATEST}" .
 docker image push "${DOCKER_REPO}"
+docker image push "${DOCKER_REPO_LATEST}"
 
 # docker run -it --rm <full_docker_image_tag> bash
 # docker image inspect <full_docker_image_tag>

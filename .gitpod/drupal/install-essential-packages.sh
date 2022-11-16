7ae2e276a44a34a65e4fc530f03d9dd65c2b7dec
Ofer Shaal <shaalofer@gmail.com>
2022-11-16 03:09:12 +0000

    adding support for composer-drupal-lenient

--- .gitpod/drupal/install-essential-packages.sh
+++ .gitpod/drupal/install-essential-packages.sh
@@ -11,7 +11,11 @@ if [ "$DP_EXTRA_ADMIN_TOOLBAR" != '1' ]; then
     unset ADMIN_TOOLBAR_PACKAGE
 fi
 
+if [ "$DP_EXTRA_COMPOSER_DRUPAL_LENIENT" != '1' ]; then
+    unset COMPOSER_DRUPAL_LENIENT
+fi
+
 cd "${GITPOD_REPO_ROOT}" && time ddev composer require --dev drupal/core-dev:* -W --no-install
 cd "${GITPOD_REPO_ROOT}" && time ddev composer require --dev phpspec/prophecy-phpunit:^2 --no-install
-cd "${GITPOD_REPO_ROOT}" && time ddev composer require drush/drush:^11.0 drupal/coder:^8.3 "$DEVEL_PACKAGE" "$ADMIN_TOOLBAR_PACKAGE" --no-install
+cd "${GITPOD_REPO_ROOT}" && time ddev composer require drush/drush:^11.0 drupal/coder:^8.3 "$DEVEL_PACKAGE" "$ADMIN_TOOLBAR_PACKAGE" "$COMPOSER_DRUPAL_LENIENT" --no-install
 cd "${GITPOD_REPO_ROOT}" && time ddev composer update

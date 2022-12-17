3b4a56e74aa758500b3d9ec1399a447eda9d6cd7
Ofer Shaal <shaalofer@gmail.com>
2022-12-17 08:56:28 +0000

    fix: create missing directory

--- .gitpod/drupal/drupalpod-setup.sh
+++ .gitpod/drupal/drupalpod-setup.sh
@@ -89,6 +89,7 @@ if [ ! -f "${GITPOD_REPO_ROOT}"/.drupalpod_initiated ] && [ -n "$DP_PROJECT_TYPE
     else
         # If not core - clone selected project into /repos and remove drupal core
         rm -rf "${GITPOD_REPO_ROOT}"/repos/drupal
+        mkdir -p "${GITPOD_REPO_ROOT}"/repos
         cd "${GITPOD_REPO_ROOT}"/repos && time git clone https://git.drupalcode.org/project/"$DP_PROJECT_NAME"
     fi
 

009373e8d6a090db39757bf021acb31c50677f97
Ofer Shaal <shaalofer@gmail.com>
2023-09-04 05:21:35 +0000

    fix: composer install if web and vendor do not exist

--- .gitpod/drupal/install-essential-packages.sh
+++ .gitpod/drupal/install-essential-packages.sh
@@ -11,6 +11,13 @@ if [ "$DP_EXTRA_ADMIN_TOOLBAR" != '1' ]; then
     unset ADMIN_TOOLBAR_PACKAGE
 fi
 
+# If vendor directory doesn't exist, essential files are missing, run `composer install` to prepare web/ and vendor/ directories
+if [ ! -d "$GITPOD_REPO_ROOT"/vendor ]; then
+    echo "/vendor not found"
+    cd "$GITPOD_REPO_ROOT" && \
+    time ddev . composer install
+fi
+
 cd "${GITPOD_REPO_ROOT}" && time ddev composer require --dev drupal/core-dev:* -W --no-install
 cd "${GITPOD_REPO_ROOT}" && time ddev composer require --dev phpspec/prophecy-phpunit:^2 --no-install
 cd "${GITPOD_REPO_ROOT}" && time ddev . composer require drush/drush:^11.0 drupal/coder:^8.3 "$DEVEL_PACKAGE" "$ADMIN_TOOLBAR_PACKAGE" --no-install
